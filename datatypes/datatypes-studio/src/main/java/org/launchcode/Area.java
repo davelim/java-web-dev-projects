38873c8eb983818f1370dcbf93dc7718df873f09
Dave Lim <davelim77@gmail.com>
2024-08-01 16:23:54 -0500

    Loop until positive number for radius

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,12 +6,18 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Provide radius:");
-        if (!input.hasNextDouble()) {
-            System.out.println("Please provide a positive number.");
-            return;
+        double radius = -1;
+        while (radius <= 0) {
+            try {
+                radius = input.nextDouble();
+                if (radius <= 0)
+                    System.out.println("Please provide a _positive_ number.");
+            } catch (java.util.InputMismatchException e) {
+                input.next(); // advance pass input that cause InputMismatchException
+                System.out.println("Please provide a number.");
+            }
         }
-        double radius = input.nextDouble();
-
+        input.close();
         System.out.println("The area of a circle of radius " + radius + " is : " + studios.areaofacircle.Circle.getArea(radius));
     }
 }
