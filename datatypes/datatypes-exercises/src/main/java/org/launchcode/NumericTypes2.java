1c59959bff9dd0d35484e655a438d8c206a66982
Dave Lim <davelim77@gmail.com>
2024-08-01 12:33:32 -0500

    Miles-per-gallon

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/NumericTypes2.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class NumericTypes2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Provide miles driven:");
+        float miles = input.nextFloat();
+        System.out.println("Provide gallons consumed:");
+        float gallon = input.nextFloat();
+        System.out.println("Miles-per-gallon: " + (miles / gallon));
+    }
+}
