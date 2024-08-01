1399512e9a97f04892497fa3aff54d093da026bb
Dave Lim <davelim77@gmail.com>
2024-08-01 14:05:39 -0500

    Some basic error checking

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,7 +6,12 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Provide radius:");
+        if (!input.hasNextDouble()) {
+            System.out.println("Please provide a positive number.");
+            return;
+        }
         double radius = input.nextDouble();
+
         System.out.println("The area of a circle of radius " + radius + " is : " + studios.areaofacircle.Circle.getArea(radius));
     }
 }
