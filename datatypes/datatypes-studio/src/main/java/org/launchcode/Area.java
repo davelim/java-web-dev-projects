5c1851b20ddcafeb1a6baf8bc3e68874a54c70c3
Dave Lim <davelim77@gmail.com>
2024-08-01 13:53:44 -0500

    Area of a circle

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Provide radius:");
+        double radius = input.nextDouble();
+        System.out.println("The area of a circle of radius " + radius + " is : " + studios.areaofacircle.Circle.getArea(radius));
+    }
+}
