c1ef1166afcbcdbbc29453888d6947844415c186
Dave Lim <davelim77@gmail.com>
2024-08-01 11:46:03 -0500

    Add option for German

--- datatypes/datatype-examples/src/main/java/org/launchcode/HelloMethods.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/HelloMethods.java
@@ -3,7 +3,7 @@ package org.launchcode;
 public class HelloMethods {
 
     public static void main(String[] args) {
-        String message = Message.getMessage("fr");
+        String message = Message.getMessage("de");
         String helloOnly = message.substring(0, message.indexOf(','));
         System.out.println(helloOnly);
     }
